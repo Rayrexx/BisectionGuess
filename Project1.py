34e456cce028334d5ed39a42676267ec6c241678
harsh <harshkaushik1997@gmail.com>
2020-02-25 12:33:33 +0530

    First commit

--- Project1.py
+++ Project1.py
@@ -1,11 +1,11 @@
 high = 100
 low = 0
 print("Please think of a number between 0 and 100!")
-input("Press 'x' to continue")
+input("Press 'x' to continue ")
 guess = int((high + low)/2)
 while guess <= 100:
     print("Is your secret number ",guess,"?")
-    inp = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")
+    inp = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. ")
     if inp == 'h':
         high = guess
         guess = round((high + low)/2)
